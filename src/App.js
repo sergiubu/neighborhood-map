edd9357f5fbb5da9c20c7d8d718416ae9d10095e
Sergiu <sil3ent@yahoo.com>
2018-07-05 12:43:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Map from './components/Map'
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
