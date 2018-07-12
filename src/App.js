2514f6afc7c718b26f811d1a540a892906d54353
Sergiu <sil3ent@yahoo.com>
2018-07-12 12:17:50 +0300

    Re-add markers & testing info windows

--- src/App.js
+++ src/App.js
@@ -5,18 +5,61 @@ import './App.css'
 export default class App extends Component {
 
   state = {
-    map: ''
+    map: '',
+    markers: [],
+    clickedMarker: '',
+    infowindow: ''
   }
 
-  setMap = map => {
+  setMap = (map) => {
     this.setState({ map })
   }
 
+  setMarkers = (marker) => {
+    this.setState({ markers: marker })
+  }
+
+  filterMarkers = (map) => {
+    this.state.markers.forEach(marker => {
+      marker.setMap(map)
+    })
+  }
+
+  setInfoWindow = (infoWindow) => {
+    this.setState({ infowindow: infoWindow })
+  }
+
+  openInfoWindow = (marker) => {
+    // testing
+    this.closeInfoWindow();
+
+    this.state.infowindow.open(this.state.map, marker);
+    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    setTimeout(_ => {
+      marker.setAnimation(null);
+    }, 1500);
+    
+    this.setState({ clickedMarker: marker });
+    console.log('[clicked marker]', marker);
+  }
+
+  closeInfoWindow = () => {
+    // testing
+    this.state.infowindow.close();
+    
+    this.setState({ clickedMarker: '' });
+  }
+  
   render() {
     return (
       <div className="App">
         <Map
           setMap={this.setMap}
+          setMarkers={this.setMarkers}
+          setInfoWindow={this.setInfoWindow}
+          openInfoWindow={this.openInfoWindow}
+          closeInfoWindow={this.closeInfoWindow}
+          clickedMarker={this.state.clickedMarker}
         />
       </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import { locations } from './Data'
+import { locations } from './Data'
 
 class Map extends Component {
 
@@ -9,13 +9,36 @@ class Map extends Component {
     loadMapJS('https://maps.googleapis.com/maps/api/js?&key=AIzaSyB8BKW9-aUTTHXeSHIadm60hjH-h04FD4o&v=3&callback=initMap')
   }
 
+  // Initialize map
   initMap = () => {
     const mapContainer = document.getElementById('map');
     const map = new window.google.maps.Map(mapContainer, {
       center: { lat:  40.78306, lng: -73.971249},
       zoom: 13
-    })
+    });
     this.props.setMap(map);
+    
+    // Add markers to map
+		locations.forEach(location => {
+			let marker = new window.google.maps.Marker({
+				map: map,
+				position: location.coords,
+				title: location.name,
+				animation: window.google.maps.Animation.DROP
+      });
+      
+      let infoWindow = new window.google.maps.InfoWindow({
+        content: location.name
+      });
+      
+      marker.addListener('click', _ => {
+        this.props.setInfoWindow(infoWindow);
+        this.props.openInfoWindow(marker);
+        
+        //this.props.closeInfoWindow();
+        console.log('INFOwindow', infoWindow);
+      });
+		});
   }
 
   render() {
@@ -37,4 +60,3 @@ function loadMapJS(src) {
   };
   ref.parentNode.insertBefore(script, ref);
 }
-// AIzaSyB8BKW9-aUTTHXeSHIadm60hjH-h04FD4o
